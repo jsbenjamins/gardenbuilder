33c162255280b0b95ea4646b5c80d1a6c41eca59
Jeroen Benjamins <j.s.benjamins@uu.nl>
2026-04-22 16:35:01 +0200

    big update to interactive version
    this should replace the current index to an interactive version

---
+++ docs/assets/PathsGardenbuilder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="PathsGardenbuilder" tilewidth="32" tileheight="32" spacing="8" margin="4" tilecount="1350" columns="45">
+ <image source="paths.png" width="1800" height="1200"/>
+</tileset>
